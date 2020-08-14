4059d1f8a566d34d203b23059bd491aa6306f203
TobeTangMonk <69404446+TobeTangMonk@users.noreply.github.com>
2020-08-14 19:30:08 +0800

    第一次使用upload file操作
    第一次把文件从自己电脑传入Github网站

---
+++ LocateDate.java
@@ -0,0 +1,46 @@
+package Date.demo;
+//import java.time.*;
+		public class LocateDate {
+		    public static void main(String[] args){
+		        //Date
+		 
+//		        Date today=new Date();//Date类创建对象
+//		        System.out.println("today is "+today);
+//		//
+//		        //LocalDate
+//		 
+//		        //LocalDate 用静态工厂方法（factory method）创建对象
+//		        LocalDate date=LocalDate.now();
+//		        System.out.println("date is "+date);
+//		 
+//		        //自定义数据
+//		        LocalDate today1=LocalDate.now();
+//		        today1=LocalDate.of(2001,12,5);//构建一个特定日期
+//		        System.out.println("today1 is "+today1);
+//		 
+//		        //LocalDate获取年月日
+//		        int year=date.getYear();
+//		        int month=date.getMonthValue();
+//		        int day=date.getDayOfMonth();
+//		        System.out.println(year+month+day);
+//		 
+//		        //打印一个距离现在日期多少天的新日期
+//		        LocalDate NewDay=date.plusDays(1000);
+//		        System.out.println("After 1000 days later is "+NewDay);
+//		    }
+		    	
+//		    	TemporaField TemporaField=null;
+//		    	
+//		    	LocalDateTime now=LocalDate.Time.now();
+//		    	now.withYear(2020).with(Moth.JANUARY.getValue()).With(
+//		    			ChronoFiled.ALInGNED_WEEK,3).with(ChronoFiled.ALInGNED_DAY_OF_WEEK_IN_MOTH,4)
+//		    	;
+//		    	System.out.println(now);
+		    	
+		    	String s="abcdefg";
+		    	String tString=s.substring(2,5);
+		    	System.out.println(tString);
+
+	}
+
+}
